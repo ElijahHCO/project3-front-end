7e5be1fff747d25130c572c52bb0c8295415ad75
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-03-31 15:49:38 -0600

    snowboard route

--- src/Snowboard.jsx
+++ src/Snowboard.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
+import SnowboardContainer from './SnowboardContainer/SnowboardContainer';
 
 function Snowboard() {
     return (
       <div className="App">
         <h2 className="snowboard-header">Snowboards</h2>        
-
+        <SnowboardContainer></SnowboardContainer>
       </div>
     );
   }
